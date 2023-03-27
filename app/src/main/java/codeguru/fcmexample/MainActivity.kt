8936a295f9cce2a8279c8d90e5e1c5f5d43fe8e5
Code-Apprentice <codeguru42@gmail.com>
2023-03-26 21:15:33 -0600

    Add click listener to button

--- app/src/main/java/codeguru/fcmexample/MainActivity.kt
+++ app/src/main/java/codeguru/fcmexample/MainActivity.kt
@@ -2,10 +2,20 @@ package codeguru.fcmexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import codeguru.fcmexample.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.pushBtn.setOnClickListener {
+            Snackbar.make(
+                binding.root,
+                getString(R.string.sending_message),
+                Snackbar.LENGTH_LONG
+            ).show()
+        }
     }
 }
