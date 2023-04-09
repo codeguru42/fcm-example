6ae1f360067170864d5f3582e057465f32cfa6f5
Code-Apprentice <codeguru42@gmail.com>
2023-04-09 02:39:24 -0600

    Receive message

--- app/src/main/java/codeguru/fcmexample/ExampleFirebaseMessagingService.kt
+++ app/src/main/java/codeguru/fcmexample/ExampleFirebaseMessagingService.kt
@@ -2,6 +2,7 @@ package codeguru.fcmexample
 
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class ExampleFirebaseMessagingService : FirebaseMessagingService() {
     private val TAG: String = javaClass.name
@@ -14,4 +15,22 @@ class ExampleFirebaseMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         Log.d(TAG, "Refreshed token: $token")
     }
-}
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: ${remoteMessage.from}")
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+        }
+
+        // Check if message contains a notification payload.
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }}
